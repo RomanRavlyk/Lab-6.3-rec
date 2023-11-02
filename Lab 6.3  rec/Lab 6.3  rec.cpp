391cdf9f3bc3d0ceb1bc656efd557bf27fc5469e
RomanRavlyk <Roman.Ravlyk.RI.2023@lpnu.ua>
2023-11-02 12:41:27 +0200

    ver 2

--- Lab 6.3  rec/Lab 6.3  rec.cpp
+++ Lab 6.3  rec/Lab 6.3  rec.cpp
@@ -3,9 +3,17 @@
 
 using namespace std;
 
-void squareArray(int* inputArray, int length, int i);
+
 void printArray(int* array, int size, int i);
 
+template <typename T>
+T squareArray(T* inputArray, int length, int i) {
+    if (i < length) {
+        inputArray[i] = inputArray[i] * inputArray[i];
+        return squareArray(inputArray, length, i + 1);
+    }
+    return inputArray[i];
+}
 int main() {
     int a[] = { 4, 6, 7, 6, 4, 2, 1, 2, 8, 9 };
     int n = sizeof(a) / sizeof(a[0]);
@@ -20,13 +28,7 @@ int main() {
     return 0;
 }
 
-void squareArray(int* inputArray, int length, int i) {
-    if (i < length) {
-        inputArray[i] = inputArray[i] * inputArray[i];
-    }
-    else return;
-    return (inputArray[i], squareArray(inputArray, length, i + 1));
-}
+
 
 void printArray(int* array, int size, int i) {
     if (i < size) {
